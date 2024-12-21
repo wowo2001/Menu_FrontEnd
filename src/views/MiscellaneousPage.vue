c38197de5a934aac531be919e28e2061f72952cf
wowo2001 <107996479+wowo2001@users.noreply.github.com>
2024-12-21 18:00:01 +1100

    Change miscellaneous page layout

--- src/views/MiscellaneousPage.vue
+++ src/views/MiscellaneousPage.vue
@@ -1,43 +1,29 @@
 ﻿<template>
-    <div class="header">
+  <div class="header">
     <h1>杂项</h1>
-   
-    </div>
+  </div>
+
   <div class="container">
-    <!-- Table header -->
-    <div class="row">
-      <div class="col">
-        <strong>名称</strong>
-      </div>
-      <div class="col">
-        <strong>数量</strong>
-      </div>
-      <div class="col">
-        <strong>单位</strong>
-      </div>
-    </div>
 
     <!-- Dynamically generated rows for inputs -->
     <div v-for="(row, index) in rows" :key="index" class="row">
-      <div class="col">
-        <input type="text" v-model="row.name" placeholder="Enter name" />
+      <div class="col-12 col-md-4">
+        <input type="text" v-model="row.name" class="form-control custom-input" placeholder="名称" />
       </div>
-      <div class="col">
-        <input type="number" v-model="row.amount" placeholder="Enter amount" />
+      <div class="col-12 col-md-4">
+        <input type="number" v-model="row.amount" class="form-control custom-input" placeholder="数量" />
       </div>
-      <div class="col">
-        <input type="text" v-model="row.unit" placeholder="Enter unit" />
+      <div class="col-12 col-md-4">
+        <input type="text" v-model="row.unit" class="form-control custom-input" placeholder="单位" />
       </div>
     </div>
 
-      <div class="container">
-    <!-- Button to add new row -->
-    <button @click="addRow" class="btn btn-primary mt-3">增加更多</button>
-    <button @click="submit" class="btn btn-primary mt-3 custom-margin">下一页</button>
+    <!-- Buttons -->
+    <div class="container">
+      <button @click="addRow" class="btn btn-primary mt-3">增加更多</button>
+      <button @click="submit" class="btn btn-primary mt-3 custom-margin">下一页</button>
     </div>
   </div>
 </template>
 
-<script src="./MiscellaneousPage.js"></script> <!-- Link to the JS file -->
-
-
+<script src="./MiscellaneousPage.js"></script>
