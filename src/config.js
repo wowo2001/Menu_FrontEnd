40c2b249b94ccf10702b6cea91b661c68e6a391f
wowo2001 <107996479+wowo2001@users.noreply.github.com>
2025-03-01 16:20:29 +1100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const menu_backend_url = "http://13.236.194.94:3000";
-const authentication_url = "http://13.236.194.94:4000";
+const menu_backend_url = "http://3.106.114.202:3000";
+const authentication_url = "http://3.106.114.202:4000";
 export default {
     menu_backend_url,
     authentication_url,
